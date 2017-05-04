857e0bbab9419c30e1cdbf6807e939dcef063cc8
Johannes Thiel <Johannes.Thiel@hhu.de>
2017-05-04 14:33:04 +0200

    The profile page displys basic user info.

--- app/js/UI/profileUI.js
+++ app/js/UI/profileUI.js
@@ -1,5 +1,6 @@
 const commonUI = require('./commonUI');
 const auth = require('../auth');
+const userLib = require('../user');
 
 $(() => {
   commonUI.bindEvents();
@@ -7,4 +8,41 @@ $(() => {
   auth.addAuthStateListener(commonUI.authStateListener);
 
   auth.registerAuthStateListeners();
+
+  loadUser();
 });
+
+
+function getUserId() {
+  // The path looks like /profile/$uid
+  const path = window.location.pathname.split('/');
+
+  return path[2];
+}
+
+function loadUser() {
+  const userId = getUserId();
+  commonUI.showLoadingSpinner('#profileInfoPanel');
+
+  userLib.getUser(userId, (userSnapshot) => {
+    const user = userSnapshot.val();
+
+    if (user) {
+      showUser(user);
+      commonUI.hideLoadingSpinner('#profileInfoPanel');
+    } else {
+      /* TODO User does not exists or got deleted
+       * Display a error to the user
+       */
+    }
+  });
+}
+
+function showUser(user) {
+  $('#profileUsername').text(user.username);
+  $('#profileUserBio').text(user.bio);
+
+  if (user.profileImg) {
+    $('#profileUserImage').attr('src', user.profileImg);
+  }
+}
